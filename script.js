e44668894a1eda20e078f38676afaf39438e8e58
iamtburger <tamas.burger@gmail.com>
2020-11-14 19:47:09 +0100

    ai probably works

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Gameboard object
 const gameBoard = (() => {
-    let board = ['','','','','','','','',''];
+    // let board = ['','','','','','','','',''];
+    let board = ['X','','O','O','','O','X','',''];
 
     let renderBoard = () => {
         return board.map((val, index) => {
@@ -11,9 +12,9 @@ const gameBoard = (() => {
     };
 
     let clearBoard = () => {
-        for (let i = 0; i < 9; i++) {
-            board[i] = '';
-        };
+        // for (let i = 0; i < 9; i++) {
+        //     board[i] = '';
+        // };
         document.querySelector('.grid').innerHTML = renderBoard()
 
     }
@@ -31,18 +32,105 @@ function player(name, sign, num) {
     return { playerName, playerSign, playerNum }
 }
 
+const computerAi = (() => {
+    let depth = 0;
 
-// let playerOne;
-// let playerTwo;
+    const remainingSteps = function(board) {
+        let emptyCells = []
+        for (let i = 0; i < board.length; i++) {
+            if (!board[i]) {
+                emptyCells.push(i);
+            }
+        }
+        return emptyCells;
+    };
+
+
+    function minimax (newBoard, player) {
+        depth++;
+
+        const humanPlayer = game.getPlayer()
+        let availableSlots = remainingSteps(newBoard);
+
+        // console.log('depth: '+depth)
+        // console.log('board: '+newBoard)
+        // console.log('available slots: '+availableSlots)
+
+        if (game.checkForWin(newBoard, humanPlayer)) {
+            // console.log(-10)
+            return -10;
+        } else if (game.checkForWin(newBoard, game.playerAi)) {
+            // console.log(10)
+            return 10;
+        } else if (game.checkForTie(newBoard)) {
+            // console.log(0)
+            return 0;
+        }
+        
+        const moves = [];
+
+        for (let i = 0; i < availableSlots.length; i++) {
+            let move = {};
+            // console.log('current slot: '+availableSlots[i])
+            // console.log('current player: '+player.playerSign)
+            move.index = availableSlots[i];
+            newBoard[availableSlots[i]] = player.playerSign;
+            // console.log('before:'+newBoard)
+            // console.log('selected: '+newBoard[availableSlots[i]])
+
+            if (player === game.playerAi) {
+                let result = minimax(newBoard, humanPlayer);
+                move.score = result;
+            } else {
+                let result = minimax(newBoard, game.playerAi);
+                move.score = result;
+            }
+
+            newBoard[availableSlots[i]] = "";
+            // console.log('after:'+newBoard)
+
+            // console.log(move)
+            moves.push(move);
+        };
+
+        let bestMove;
+        if (player === game.playerAi) {
+            let bestScore = -100;
+            for (let i = 0; i < moves.length; i++) {
+                if (moves[i].score > bestScore) {
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        } else {
+            let bestScore = 100;
+            for (let i = 0; i < moves.length; i++) {
+                if (moves[i].score < bestScore) {
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        }
+        // console.log(moves)
+        console.log(moves[bestMove])
+        return moves[bestMove]
+    }
+    // checking if it works till this step
+    // minimax(gameBoard.board, game.playerAi)
+
+    return {minimax}
+})()
+
 
 // Game logic and control
 const game = (() => {
     let round = 1;
     let playerOne;
     let playerTwo;
+    let playerAi = player('Skynet', 'O', 2)
 
     let getPlayer = () => {
-        console.log(playerOne)
+        return playerOne;
     }
     
 
@@ -55,36 +143,53 @@ const game = (() => {
 
 
 
-
+    const checkForTie = (board) => {
+            let emptyCells = []
+            for (let i = 0; i < board.length; i++) {
+                if (!board[i]) {
+                    emptyCells.push(i);
+                }
+            }
+            emptyCells;
+        ;
+        if (emptyCells.length === 0) {
+            return true
+        }
+    }
 
     
-    const checkForWin = (val) => {
+    const checkForWin = (board, player) => {
         // Solution using the Magic Square for 3x3 table
         const magicSquare = [8, 1, 6, 3, 5, 7, 4, 9, 2];
+        const c = player.playerSign
         // Stores the possible winning combinations
         const winningStates = [
-            [val.playerSign,val.playerSign,val.playerSign,'','','','','',''],
-            ['','','',val.playerSign,val.playerSign,val.playerSign,'','',''],
-            ['','','','','','',val.playerSign,val.playerSign,val.playerSign],
-            [val.playerSign,'','','',val.playerSign,'','','',val.playerSign],
-            ['','',val.playerSign,'',val.playerSign,'',val.playerSign,'',''],
-            [val.playerSign,'','',val.playerSign,'','',val.playerSign,'',''],
-            ['',val.playerSign,'','',val.playerSign,'','',val.playerSign,''],
-            ['','',val.playerSign,'','',val.playerSign,'','',val.playerSign],
+            [c,c,c,'','','','','',''],
+            ['','','',c,c,c,'','',''],
+            ['','','','','','',c,c,c],
+            [c,'','','',c,'','','',c],
+            ['','',c,'',c,'',c,'',''],
+            [c,'','',c,'','',c,'',''],
+            ['',c,'','',c,'','',c,''],
+            ['','',c,'','',c,'','',c],
         ];
 
         for (i = 0; i < winningStates.length; i++) {
             let winNum = 0;
             for (j = 0; j < winningStates[i].length; j++) {
-                if (gameBoard.board[j] === val.playerSign && winningStates[i][j] === val.playerSign) {
+                if (board[j] === c && winningStates[i][j] === c) {
                     winNum = winNum + magicSquare[j]
                 }
             }
             if (winNum === 15) {
-                gameWon(val.playerName)
+                return true
             }
-        }
 
+        }
+        return false
+        // if (round === 9) {
+        //     tie()
+        // }
 
         // Solution using if else statements
         // if (gameBoard.board[0] === val && gameBoard.board[1] === val && gameBoard.board[2] === val) {
@@ -118,19 +223,41 @@ const game = (() => {
         document.querySelector('#new-game').addEventListener('click', startGame)
     }
 
+    const tie = () => {
+        document.querySelector('.play-area').innerHTML = `
+        <div>
+            <h1>It's a tie! Play again?</h1>
+        </div>
+        <div>
+            <button id="new-game">Play Again</button>
+        </div>
+    `
+    document.querySelector('#new-game').addEventListener('click', startGame)
+    }
+
 
     function turn() {
         if (round % 2 !== 0) {
             this.innerText = playerOne.playerSign;
             gameBoard.board[this.dataset.label] = playerOne.playerSign;
             this.removeEventListener('click', turn);
-            checkForWin(playerOne)
+            let best = computerAi.minimax(gameBoard.board, playerAi);
+            if (checkForWin(gameBoard.board, playerOne)) {
+                gameWon(playerOne.playerName)
+            } else if (checkForTie(gameBoard.board)) {
+                tie()
+            }
             round++
+            
         } else {
             this.innerText = playerTwo.playerSign;
             gameBoard.board[this.dataset.label] = playerTwo.playerSign;
             this.removeEventListener('click', turn);
-            checkForWin(playerTwo)
+            if (checkForWin(gameBoard.board, playerTwo)) {
+                gameWon(playerTwo.playerName)
+            } else if (checkForTie(gameBoard.board)) {
+                tie()
+            }
             round++
         }
     }
@@ -176,7 +303,7 @@ const game = (() => {
 
     
 
-    return { getPlayer }
+    return { getPlayer, checkForWin, playerAi, playerTwo, checkForTie }
 
 })()
 
